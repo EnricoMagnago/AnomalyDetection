d0b54c4457ea2b641e8246b7f9ec97aa4df706f0
gianLuke <sanar@live.it>
2018-03-22 21:54:37 +0100

    Implemented SAX till smoothing (included)

---
+++ sax_implementation/SAX.py
@@ -0,0 +1,70 @@
+import numpy as np
+from saxpy.sax import sax_via_window
+import DataLoader
+import DataTypes
+import collections
+
+#parameters for SAX
+alphabet_size = 14
+paa_size = 4
+win_size = 20
+threshold_freq = 0.07
+
+def get_string_representation(dict):
+	
+	string = ""
+	for key in dat.keys():
+		string = string+key
+
+	return string
+
+def smoothing(string):
+
+	#get charachters and counter (occurences)
+	char_counter = collections.Counter(string)
+	
+	total_characthers = len(string)
+	
+	#dictionary storing item {characters:frequency}
+	dict_alphabet_frequency = {}
+
+	#string containing final smoothed string
+	string_smoothed = ""
+
+	alphabetM='';
+	countM=0;
+
+	for alphabet, count in char_counter.items():
+		freq = count/total_characthers
+		dict_alphabet_frequency[alphabet] = freq
+		if freq > countM:
+			alphabetM = alphabet
+			countM = freq
+
+	#this is implement the replacing step in the paper
+	for alphabet in string:
+		if ((dict_alphabet_frequency[alphabet] > threshold_freq) and (alphabet != alphabetM)):
+			string_smoothed = string_smoothed + alphabetM
+		else:
+			string_smoothed = string_smoothed + alphabet
+
+	return (string_smoothed)
+
+
+
+#loading data
+loader = DataLoader.DataLoader("../../dataset/")
+data = DataTypes.Data()
+#loader.load_all(data,200)
+loader.load_subset(data,2000,100)
+
+#get measures for oxygen
+tank_j_oxigen = [data.measures[i][0][1][0] for i in range(0, len(data.measures))]
+
+dat = sax_via_window(tank_j_oxigen, win_size, paa_size, alphabet_size, nr_strategy = 'exactly', z_threshold = 0.01)
+
+string = get_string_representation(dat)
+
+string_smoothed = smoothing(string)
+
+print(string_smoothed)
